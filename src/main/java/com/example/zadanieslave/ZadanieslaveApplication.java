4e26f257cf93cac1851188a3bc9c8e78df2f0d47
MandarinkaMSD-dev <vip.az.0096470@gmail.com>
2026-04-11 19:15:22 +0300

    1

---
+++ src/main/java/com/example/zadanieslave/ZadanieslaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.zadanieslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZadanieslaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZadanieslaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zadanieslave/ZadanieslaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zadanieslave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZadanieslaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
